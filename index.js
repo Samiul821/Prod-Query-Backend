d8691782de7437f80d628fae587fbd3c2d1764fa
Samiul821 <smsamiul821@gmail.com>
2025-07-01 11:08:36 +0600

    Fix CORS

--- index.js
+++ index.js
@@ -12,12 +12,24 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 // middleware
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://prod-query-e68b6.web.app", // তোমার frontend-এর production URL
+];
+
 app.use(
   cors({
-    origin: ["https://prod-query-e68b6.web.app"],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -222,10 +234,10 @@ async function run() {
     );
 
     // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    // console.log(
-    //   "Pinged your deployment. You successfully connected to MongoDB!"
-    // );
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
